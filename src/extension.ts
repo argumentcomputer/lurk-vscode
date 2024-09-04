c57ff529d85dd7b02eeb30c75e464ceedd0e53ef
Arthur Paulino <arthurleonardo.ap@gmail.com>
2024-09-03 21:03:35 -0300

    chore: several improvements
    * Remove unnecessary asynchronous code from the extension logic
    * Update the built-ins known for syntax highlight
    * Fix built-in regex so that `foo-lambda` doesn't match for "lambda"
    * Place meta commands in a different category for syntax highlight
    * Syntax highlighting for characters
    * Get rid of vulnerabilities reported when installing the extension

--- src/extension.ts
+++ src/extension.ts
@@ -5,42 +5,12 @@ import * as vscode from 'vscode';
 const lurkTerminalName = 'Lurk REPL';
 
 let lurkTerminal: vscode.Terminal | null = null;
-let textQueue: string[];
-let onLoop = false;
 
-function queueLoop() {
-    if (lurkTerminal !== null) {
-        const text = textQueue.shift();
-        if (text) {
-            lurkTerminal.sendText(text);
-        }
-        if (textQueue.length > 0) {
-            // keep popping messages
-            onLoop = true;
-            setTimeout(queueLoop, 10);
-        } else {
-            onLoop = false;
-        }
-    } else {
-        onLoop = true;
-        setTimeout(queueLoop, 100);
-    }
-}
-
-function triggerLoop() {
-    if (onLoop) {
-        return;
-    }
-    queueLoop();
-}
-
-async function createLurkTerminal() {
+function getTerminal(): vscode.Terminal {
     // create lurk terminal if it doesn't exist
     if (lurkTerminal === null || lurkTerminal.exitStatus !== undefined) {
         const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('lurkREPL');
         const lurkCommand = config.get("lurkRunCommand", "/home/user/.cargo/bin/lurk");
-        
-        textQueue = [];
 
         const terminalOptions = {
             name: lurkTerminalName,
@@ -50,29 +20,27 @@ async function createLurkTerminal() {
 
         lurkTerminal = vscode.window.createTerminal(terminalOptions);
         lurkTerminal.show(true);
-    } 
+    }
+    return lurkTerminal;
 }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    function activateLurk () {
-        createLurkTerminal();
-        vscode.window.showInformationMessage("Lurk REPL activated");
+    function start () {
+        getTerminal();
     };
 
-    async function sendSelected () {
+    function sendSelected () {
         const editor = vscode.window.activeTextEditor;
         if (editor) {
-            await createLurkTerminal();
             let text = editor.document.getText(editor.selection);
-            console.log("Sending to lurk REPL: %s", text);
-            textQueue.push(text);
-            triggerLoop();
+            let terminal = getTerminal();
+            terminal.sendText(text);
         }
     };
 
-    context.subscriptions.push(vscode.commands.registerCommand('lurkREPL.activate', activateLurk));
+    context.subscriptions.push(vscode.commands.registerCommand('lurkREPL.start', start));
     context.subscriptions.push(vscode.commands.registerCommand('lurkREPL.sendSelected', sendSelected));
  }
 
